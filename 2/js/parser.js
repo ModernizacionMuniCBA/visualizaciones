0d451dca3d7671f0b359e82dd1d96f0b3046b401
noeliablopez <noelia.lopez@wolox.com.ar>
2016-07-25 09:52:02 -0300

    Draw function

--- 2/js/parser.js
+++ 2/js/parser.js
@@ -1,7 +1,12 @@
 $( document ).ready(function() {
 
   d3.json("data/page.json", function(error, json) {
-  	console.log(json);
+  	const funcionarios = json.results;
+  	console.log(funcionarios);
+  	$.each(funcionarios, function(indexFuncionario, funcionario) {
+  		console.log(funcionario);
+  	});
+  		
   });
 
 });
