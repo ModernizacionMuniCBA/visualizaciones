df2b0e39fc11d0613dc47b72f796bfcf39cc17dd
noeliablopez <noelia.lopez@wolox.com.ar>
2016-07-25 11:44:28 -0300

    Parser pending

--- 2/js/parser.js
+++ 2/js/parser.js
@@ -1,12 +1,36 @@
+var funcionarios;
+var data;
+var count;
+
 $( document ).ready(function() {
 
   d3.json("data/page.json", function(error, json) {
-  	const funcionarios = json.results;
-  	console.log(funcionarios);
-  	$.each(funcionarios, function(indexFuncionario, funcionario) {
-  		console.log(funcionario);
-  	});
-  		
-  });
+  	funcionarios = json.results;
 
+  	var firstElem = _.find(funcionarios, function(o) { return o.cargo['depende_de'] == null; });
+  	data = {
+  		'name': firstElem.funcionario.nombre + ' ' + firstElem.funcionario.apellido,
+  		'object': firstElem,
+  		'children': []
+  	};
+  	count = 1;
+  	searchDependencies(firstElem.id, data);
+  	console.log(data);
+  	console.log(count);
+  	draw(data);
+  });
 });
+
+function searchDependencies(id, root) {
+	const dependencies = _.filter(funcionarios, function(o) { return o.cargo['depende_de'] == id; });
+	$.each(dependencies, function(indexDepency, dependency) {
+		count++;
+		root.children.push({
+  		'name': dependency.funcionario.nombre + ' ' + dependency.funcionario.apellido,
+  		'object': dependency,
+  		'size': 20,
+  		'children': []
+		})
+		searchDependencies(dependency.id, root.children[indexDepency]);
+	});
+}
