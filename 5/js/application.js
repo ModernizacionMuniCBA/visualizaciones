d186d8947647b787e21b1970c6a9cca41ebd913d
Diego Strubolini <strubolini@gmail.com>
2016-08-16 19:06:14 -0300

    Toma datos de la url, no de archivo

--- 5/js/application.js
+++ 5/js/application.js
@@ -8,18 +8,7 @@ var countEdad = 0;
 var chartNameMap;
 
 var apiUrl = "https://gobiernoabierto.cordoba.gov.ar";
-// $.ajax({
-//     type: "GET",
-//     url: apiUrl + "/api/funciones/?format=json&page_size=350",
-//     dataType: "jsonp",
-//     done: function(data){
-//         console.log(data);
-//     },
-//     error: function(err) { console.log(err); }
-// });
-// loadJson("https://gobiernoabierto.cordoba.gov.ar/api/funciones/?format=json&page_size=350");
-
-loadJson("page.json");
+loadJson(apiUrl + "/api/funciones/?format=json&page_size=350");
 
 function loadJson(path) {
     secretaryChart = dc.rowChart('#secretary-chart');
@@ -35,238 +24,242 @@ function loadJson(path) {
         "genero": genderChart,
         "cargo": rankChart
     };
-    d3.json(path, function (data) {
-        var dateFormat = d3.time.format('%Y-%m-%d');
-        var numberFormat = d3.format('.2f');
-        var topDir = 10;
-        var totalWidth = 990;
-        var height = 300;
+    d3.json(path, function (error,data) {
+        if(error){
+            $("#content").html("Hubo un error al cargar los datos");
+        } else {
+            var dateFormat = d3.time.format('%Y-%m-%d');
+            var numberFormat = d3.format('.2f');
+            var topDir = 10;
+            var totalWidth = 990;
+            var height = 300;
 
-        var maxYear = new Date().getFullYear();
-        var minYear = maxYear;
+            var maxYear = new Date().getFullYear();
+            var minYear = maxYear;
 
-        var people = data.results;
-        people.map(function (d) {
-            if (d.fecha_inicio) {
-                d.fechaIni = dateFormat.parse(d.fecha_inicio).getMonth() + 1;
-            }
-            if (!d.funcionario.franjaetaria) {
-                d.funcionario.franjaetaria = "Desconocido";
-            }
-            if (!d.funcionario.genero) {
-                d.funcionario.genero = "No especificado";
-            }
-            if (!d.funcionario.edad) {
-                d.funcionario.edad = null;
-            }
-            return d;
-        });
-
-        people = people.filter(function (person) {
-            return (person.cargo.depende_de == null || person.cargo.superioresids.length != 0);
-        });
-
-        var root = getSubordinates(people, null)[0];
-        root.secretary = "Intendencia";
-        var directSubs = getSubordinates(people, root.cargo.id);
-        directSubs.forEach(function (val) {
-            val.secretary = val.cargo.oficina;
-            loadSecretary(people, val.cargo.id, val.secretary);
-        });
-
-        loadDefaultValues(people, root.id);
-
-
-        var peopleData = crossfilter(people);
-        var all = peopleData.groupAll();
-
-        var ageDimension = peopleData.dimension(function (d) {
-            return d.funcionario.franjaetaria;
-        });
-
-        var dateDimension = peopleData.dimension(function (d) {
-            return d.fechaIni;
-        });
-
-        var rankDimension = peopleData.dimension(function (d) {
-            return d.cargo.categoria.nombre;
-        });
-
-        var secretaryDimension = peopleData.dimension(function (d) {
-            return d.secretary;
-        });
-
-
-        var genderDimension = peopleData.dimension(function (d) {
-            return d.funcionario.genero;
-        });
-
-        var allDimension = peopleData.dimension(function (d) {
-            return "";
-        });
-
-        var ageGroup = ageDimension.group().reduceCount();
-
-        var genderGroup = genderDimension.group().reduceCount();
-
-        var dateGroup = dateDimension.group().reduceCount();
-
-        var rankGroup = rankDimension.group().reduceCount();
-
-        var secretaryGroup = secretaryDimension.group().reduceCount();
-
-        var average_age_group = allDimension.group().reduce(
-            function reduceAdd(ka, v) {
-                if (v.funcionario.edad == null) {
-                    return ka;
+            var people = data.results;
+            people.map(function (d) {
+                if (d.fecha_inicio) {
+                    d.fechaIni = dateFormat.parse(d.fecha_inicio).getMonth() + 1;
                 }
-                ++ka.count;
-                ka.total += v.funcionario.edad;
-                return ka;
-            },
-
-            function reduceRemove(ka, v) {
-                if (v.funcionario.edad == null) {
-                    return ka;
+                if (!d.funcionario.franjaetaria) {
+                    d.funcionario.franjaetaria = "Desconocido";
                 }
-                --ka.count;
-                ka.total -= v.funcionario.edad;
-                return ka;
-            },
-
-            function reduceInitial() {
-                return {count: 0, total: 0};
-            }
-        );
-
-        rankChart
-            .width(totalWidth / 3)
-            .height(300)
-            .margins({top: 20, left: 10, right: 10, bottom: 20})
-            .dimension(rankDimension)
-            .ordinalColors(["#6baed6"])
-            .renderLabel(true)
-            .legend(dc.legend().x(400).y(10).itemHeight(13).gap(5))
-            .gap([1])
-            .group(rankGroup)
-            .elasticX(true)
-            .xAxis().tickFormat(d3.format("d"));
-
-        secretaryChart
-            .width(totalWidth / 3)
-            .height(300)
-            .margins({top: 20, left: 10, right: 10, bottom: 20})
-            .dimension(secretaryDimension)
-            .ordinalColors(["#6baed6"])
-            .renderLabel(true)
-            .legend(dc.legend().x(400).y(10).itemHeight(13).gap(5))
-            .gap([1])
-            .group(secretaryGroup)
-            .elasticX(true)
-            .xAxis().tickFormat(d3.format("d"));
-
-        averageAge.group(average_age_group)
-            .valueAccessor(function (p) {
-                if (p.value.count) {
-                    return (p.value.total) / p.value.count;
-                } else {
-                    return -1;
+                if (!d.funcionario.genero) {
+                    d.funcionario.genero = "No especificado";
                 }
-            })
-            .formatNumber(function (d) {
-                if (d == -1) {
-                    return "N/A";
+                if (!d.funcionario.edad) {
+                    d.funcionario.edad = null;
                 }
-                return d.toFixed(1);
+                return d;
             });
 
-        ageChart
-            .width(totalWidth / 4)
-            .height(300)
-            .margins({top: 20, left: 10, right: 10, bottom: 20})
-            .dimension(ageDimension)
-            .ordinalColors(['#6baed6'])
-            .renderLabel(true)
-            .group(ageGroup)
-            .elasticX(true)
-            .xAxis().tickFormat(d3.format("d"));
-
-        genderChart
-            .width(totalWidth / 5)
-            .height(300)
-            .dimension(genderDimension)
-            .ordinalColors(['#e377c2', "#1f77b4", "#DDD"])
-            .renderLabel(true)
-            .group(genderGroup);
-
-        peopleCount /* dc.dataCount('.dc-data-count', 'chartGroup'); */
-            .dimension(peopleData)
-            .group(all)
-            .html({
-                some: '<strong>%filter-count</strong> de <strong>%total-count</strong> ' +
-                ' | <a href=\'javascript:dc.filterAll(); history.pushState(null, null, "#[]"); dc.renderAll();\'\'>borrar todos los filtros</a>',
-                all: 'Todos los funcionarios están seleccionados.'
+            people = people.filter(function (person) {
+                return (person.cargo.depende_de == null || person.cargo.superioresids.length != 0);
             });
 
-        peopleTable
-            .dimension(ageDimension)
-            .group(function (d) {
+            var root = getSubordinates(people, null)[0];
+            root.secretary = "Intendencia";
+            var directSubs = getSubordinates(people, root.cargo.id);
+            directSubs.forEach(function (val) {
+                val.secretary = val.cargo.oficina;
+                loadSecretary(people, val.cargo.id, val.secretary);
+            });
+
+            loadDefaultValues(people, root.id);
+
+
+            var peopleData = crossfilter(people);
+            var all = peopleData.groupAll();
+
+            var ageDimension = peopleData.dimension(function (d) {
                 return d.funcionario.franjaetaria;
-            })
-            .size(400)
-            .columns([
-                {
-                    label: 'Foto',
-                    format: function (d) {
-                        if (!d.funcionario.foto.thumbnail)
-                            return "";
-                        return "<img class='profile-picture' src='" + d.funcionario.foto.thumbnail + "'/>";
+            });
+
+            var dateDimension = peopleData.dimension(function (d) {
+                return d.fechaIni;
+            });
+
+            var rankDimension = peopleData.dimension(function (d) {
+                return d.cargo.categoria.nombre;
+            });
+
+            var secretaryDimension = peopleData.dimension(function (d) {
+                return d.secretary;
+            });
+
+
+            var genderDimension = peopleData.dimension(function (d) {
+                return d.funcionario.genero;
+            });
+
+            var allDimension = peopleData.dimension(function (d) {
+                return "";
+            });
+
+            var ageGroup = ageDimension.group().reduceCount();
+
+            var genderGroup = genderDimension.group().reduceCount();
+
+            var dateGroup = dateDimension.group().reduceCount();
+
+            var rankGroup = rankDimension.group().reduceCount();
+
+            var secretaryGroup = secretaryDimension.group().reduceCount();
+
+            var average_age_group = allDimension.group().reduce(
+                function reduceAdd(ka, v) {
+                    if (v.funcionario.edad == null) {
+                        return ka;
                     }
+                    ++ka.count;
+                    ka.total += v.funcionario.edad;
+                    return ka;
                 },
-                {
-                    label: 'Funcionario',
-                    format: function (d) {
-                        return "<a href=" + apiUrl + d.funcionario.url + ">" + getText(d.funcionario) + "</a>";
-                        function getText(func) {
-                            if (func.nombrepublico) {
-                                return func.nombrepublico;
+
+                function reduceRemove(ka, v) {
+                    if (v.funcionario.edad == null) {
+                        return ka;
+                    }
+                    --ka.count;
+                    ka.total -= v.funcionario.edad;
+                    return ka;
+                },
+
+                function reduceInitial() {
+                    return {count: 0, total: 0};
+                }
+            );
+
+            rankChart
+                .width(totalWidth / 3)
+                .height(300)
+                .margins({top: 20, left: 10, right: 10, bottom: 20})
+                .dimension(rankDimension)
+                .ordinalColors(["#6baed6"])
+                .renderLabel(true)
+                .legend(dc.legend().x(400).y(10).itemHeight(13).gap(5))
+                .gap([1])
+                .group(rankGroup)
+                .elasticX(true)
+                .xAxis().tickFormat(d3.format("d"));
+
+            secretaryChart
+                .width(totalWidth / 3)
+                .height(300)
+                .margins({top: 20, left: 10, right: 10, bottom: 20})
+                .dimension(secretaryDimension)
+                .ordinalColors(["#6baed6"])
+                .renderLabel(true)
+                .legend(dc.legend().x(400).y(10).itemHeight(13).gap(5))
+                .gap([1])
+                .group(secretaryGroup)
+                .elasticX(true)
+                .xAxis().tickFormat(d3.format("d"));
+
+            averageAge.group(average_age_group)
+                .valueAccessor(function (p) {
+                    if (p.value.count) {
+                        return (p.value.total) / p.value.count;
+                    } else {
+                        return -1;
+                    }
+                })
+                .formatNumber(function (d) {
+                    if (d == -1) {
+                        return "N/A";
+                    }
+                    return d.toFixed(1);
+                });
+
+            ageChart
+                .width(totalWidth / 4)
+                .height(300)
+                .margins({top: 20, left: 10, right: 10, bottom: 20})
+                .dimension(ageDimension)
+                .ordinalColors(['#6baed6'])
+                .renderLabel(true)
+                .group(ageGroup)
+                .elasticX(true)
+                .xAxis().tickFormat(d3.format("d"));
+
+            genderChart
+                .width(totalWidth / 5)
+                .height(300)
+                .dimension(genderDimension)
+                .ordinalColors(['#e377c2', "#1f77b4", "#DDD"])
+                .renderLabel(true)
+                .group(genderGroup);
+
+            peopleCount /* dc.dataCount('.dc-data-count', 'chartGroup'); */
+                .dimension(peopleData)
+                .group(all)
+                .html({
+                    some: '<strong>%filter-count</strong> de <strong>%total-count</strong> ' +
+                    ' | <a href=\'javascript:dc.filterAll(); history.pushState(null, null, "#[]"); dc.renderAll();\'\'>borrar todos los filtros</a>',
+                    all: 'Todos los funcionarios están seleccionados.'
+                });
+
+            peopleTable
+                .dimension(ageDimension)
+                .group(function (d) {
+                    return d.funcionario.franjaetaria;
+                })
+                .size(400)
+                .columns([
+                    {
+                        label: 'Foto',
+                        format: function (d) {
+                            if (!d.funcionario.foto.thumbnail)
+                                return "";
+                            return "<img class='profile-picture' src='" + d.funcionario.foto.thumbnail + "'/>";
+                        }
+                    },
+                    {
+                        label: 'Funcionario',
+                        format: function (d) {
+                            return "<a href=" + apiUrl + d.funcionario.url + ">" + getText(d.funcionario) + "</a>";
+                            function getText(func) {
+                                if (func.nombrepublico) {
+                                    return func.nombrepublico;
+                                }
+                                return func.nombre + " " + func.apellido;
                             }
-                            return func.nombre + " " + func.apellido;
+                        }
+                    },
+                    {
+                        label: 'Edad',
+                        format: function (d) {
+                            return d.funcionario.edad;
+                        }
+                    },
+                    {
+                        label: 'Cargo',
+                        format: function (d) {
+                            return d.cargo.categoria.nombre;
+                        }
+                    },
+                    {
+                        label: 'Oficina',
+                        format: function (d) {
+                            return d.cargo.oficina;
                         }
                     }
-                },
-                {
-                    label: 'Edad',
-                    format: function (d) {
-                        return d.funcionario.edad;
-                    }
-                },
-                {
-                    label: 'Cargo',
-                    format: function (d) {
-                        return d.cargo.categoria.nombre;
-                    }
-                },
-                {
-                    label: 'Oficina',
-                    format: function (d) {
-                        return d.cargo.oficina;
-                    }
-                }
-            ])
-            .order(d3.ascending)
-            .sortBy(function (d) {
-                return d.funcionario.edad;
-            })
-            .on('renderlet', function (table) {
-                table.selectAll('.dc-table-group').classed('info', true);
-            });
-        
-        dcFilterRememberer();
+                ])
+                .order(d3.ascending)
+                .sortBy(function (d) {
+                    return d.funcionario.edad;
+                })
+                .on('renderlet', function (table) {
+                    table.selectAll('.dc-table-group').classed('info', true);
+                });
 
-        dc.renderAll();
+            dcFilterRememberer();
 
+            dc.renderAll();
+        }
 
+        $("#content").show();
     });
 }
 
