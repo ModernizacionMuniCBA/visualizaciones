db415ebd538810949c01de8eae9a39986026a0f1
Ariel Aizemberg <ariel.aizemberg@gmail.com>
2016-09-26 12:46:28 -0300

    Update application.js

--- dendograma/js/application.js
+++ dendograma/js/application.js
@@ -8,6 +8,7 @@ $(function() {
 function dendogram(funcionarios) {
     var selectedradius = getParameterByName("radio");
     if (!selectedradius) {
+        console.log("window.innerWidth " + window.innerWidth);
         radius = window.innerWidth;
     } else {
         radius = selectedradius;
