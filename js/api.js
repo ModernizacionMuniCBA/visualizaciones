1a1e9a479e21ce0ee8d662265b14ea234bcc1cc8
Gaston Ramirez <gastonframirez@gmail.com>
2017-06-05 10:58:34 -0300

    Arregla #506
    Agrega cache para 1 dia, se refrescan D3 si paso un dia.

--- js/api.js
+++ js/api.js
@@ -1,7 +1,9 @@
 var funcionariosUrl = "//gobiernoabierto.cordoba.gob.ar";
 
 var funcionariosTask = new Promise(function (resolve, reject) {
-    if (localStorage.funcionariosData == null || localStorage.funcionariosData == "null" || localStorage.funcionariosData == "undefined") {
+    var today = new Date().toLocaleDateString();
+
+    if (localStorage.funcionariosData == null || localStorage.funcionariosData == "null" || localStorage.funcionariosData == "undefined" || localStorage.userDate != today ) {
         d3.json(funcionariosUrl + "/api/funciones/?format=json&page_size=350", function (error, funcionarios) {
             if (error) reject(error);
             resolve(funcionarios);
@@ -10,6 +12,7 @@ var funcionariosTask = new Promise(function (resolve, reject) {
     } else {
         resolve(JSON.parse(localStorage.funcionariosData));
     }
+    localStorage.userDate = today;
 });
 
 function getApiUrl() {
